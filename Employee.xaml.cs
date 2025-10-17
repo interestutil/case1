eed35c5c4860194d3eab9eac41f4edb3222981f6
Adam <adamkriaco@gmail.com>
2025-10-17 23:00:02 +0300

    fully functional employee page now.

--- Employee.xaml.cs
+++ Employee.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using case1.Data;
+using Microsoft.IdentityModel.Tokens;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -22,15 +23,52 @@ namespace case1
     {
         string emp;
         Context context = new Context();
+        void refresh()
+        {
+            var query = from x in context.Tasks
+                         where x.Name == emp && x.Status != "Completed"
+                         select new Tasks { TaskID = x.TaskID, Title = x.Title, Description = x.Description, Status = x.Status };
+            var query2 = from x in context.Tasks
+                         where x.Name == emp && x.Status == "Completed"
+                         select new Tasks { TaskID = x.TaskID, Title = x.Title, Description = x.Description, Status = x.Status };
+            pendingdata.ItemsSource = query.ToList();
+            completeddata.ItemsSource = query2.ToList();
+        }
         public Employee(string emp_name)
         {
             InitializeComponent();
             emp = emp_name;
             emp_name_label.Content = emp;
+            refresh();
+        }
+
+        private void pendingdata_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (pendingdata.SelectedItem is Tasks t)
+            {
+                int taskId = t.TaskID;
+                tasklabel.Content = taskId.ToString();
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
             var query = (from x in context.Tasks
-                         where x.Name == emp && x.Status != "Completed"
-                         select x).Select(t => new { t.TaskID, t.Title, t.Description, t.Status });
-            pendingdata.ItemsSource = query.ToList();
+                         where x.TaskID.ToString() == tasklabel.Content.ToString()
+                         select x).FirstOrDefault();
+            if (query != null)
+            {
+                if (statuscombobox.Text.IsNullOrEmpty())
+                {
+                    MessageBox.Show("Please select a status.");
+                }
+                else
+                {
+                    query.Status = statuscombobox.Text;
+                    context.SaveChanges();
+                }
+                refresh();
+            }
         }
     }
 }
